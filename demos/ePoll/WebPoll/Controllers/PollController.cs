3e15d4fdbdd9c354af00782611feba8e333f8189
Pandu Masabathula <pandu.masabathula@gmail.com>
2013-10-13 18:56:11 +0530

    C# Demo - Console Demo - Good
    MVC 4.0 Web Application - Work In Progess

---
+++ demos/ePoll/WebPoll/Controllers/PollController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebPoll.Controllers
+{
+    using WebPoll.Models;
+    using WebPoll.Workers;
+
+    public class PollController : Controller
+    {
+        //
+        // GET: /Poll/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Welcome()
+        {
+            PollWorker worker = new PollWorker();
+            PollQuestion question = worker.GetActiveQuestion();
+            if (question == null)
+            {
+                //return HttpNotFound;
+            }
+            return View(question);
+        }
+
+        public ActionResult SaveAnswer(PollUserAnswer answer)
+        {
+            //PollUserAnswer answer = new PollUserAnswer();
+            return View();
+        }
+
+    }
+}
